89aa4594ecb691c7295b32b46241956fc8c483c7
DaRealIfrit <68937928+DaRealIfrit@users.noreply.github.com>
2021-02-16 07:30:45 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,13 +10,17 @@ function setup() {
   car.shapeColor = color(255)
   car.velocityX = speed;
 
-  wall = createSprite(1400, 200, 60, height/2);
-  wall.shapeColor = color(80, 80, 80)
+  wall = createSprite(1300, 200, 60, height/2);
+  wall.shapeColor = color("purple")
 
 }
 
 function draw() {
   background(0);
+
+  stroke("purple");
+  fill("purple");
+  text("Press 'R' to Restart", 740, 200);
 
   if(wall.x - car.x < (car.width + wall.width) /2){
     car.velocityX = 0;
@@ -33,6 +37,13 @@ function draw() {
 
   if(deformation < 100){
     car.shapeColor = color(0, 255, 0);
+  }
+
+  if(keyDown("r")){
+    car.x = 200
+    speed = random(55, 90);
+    car.velocityX = speed;
+    car.shapeColor = color(255)
   }
   drawSprites();
 }
